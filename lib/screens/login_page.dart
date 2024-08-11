dcd430f9a4da4afb4a9c10843c54d0420c5f8f7a
İsmail Babacan <114854598+ismailbabacann@users.noreply.github.com>
2024-08-11 15:34:31 +0300

    fix again

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -11,104 +11,104 @@ class LoginPage extends StatelessWidget {
   final _tEmail= TextEditingController();
   final _tPassword = TextEditingController();
 
-
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.all(32.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SvgPicture.asset(
-              'lib/assets/icons/logo_enyakın.svg',
-              height: 50,
-              fit: BoxFit.cover,
-            ),
-            SizedBox(height: 16),
-            const Text(
-              'Hesabına giriş yap',
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: 100,),
+              SvgPicture.asset(
+                'lib/assets/icons/logo_enyakın.svg',
+                height: 50,
+                fit: BoxFit.cover,
               ),
-            ),
-            SizedBox(height: 8),
-            const Text(
-              'Uygulamaya giriş yapmak için mail adresini gir',
-              style: TextStyle(fontSize: 16),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 16),
-            TextField(
-              controller: _tEmail,
-              decoration: InputDecoration(
-                labelText: 'Email',
-                border: OutlineInputBorder(
+              SizedBox(height: 16),
+              const Text(
+                'Hesabına giriş yap',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            ),
-            SizedBox(height: 16),
-             TextField(
-              controller: _tPassword,
-              decoration: InputDecoration(
-                labelText: 'Şifre',
-                border: OutlineInputBorder(),
+              SizedBox(height: 8),
+              const Text(
+                'Uygulamaya giriş yapmak için mail adresini gir',
+                style: TextStyle(fontSize: 16),
+                textAlign: TextAlign.center,
               ),
-              obscureText: true,
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                  AuthService().signIn(context, email: _tEmail.text, password: _tPassword.text);
-              },
-              child: Text('Giriş Yap' , style: TextStyle(color: Colors.white),),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: mainColor,
-                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+              SizedBox(height: 16),
+              TextField(
+                controller: _tEmail,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(
+                  ),
+                ),
               ),
-            ),
-            SizedBox(height: 16),
-            Row(
-              children: [
-                Expanded(child: Divider()),
-                Text(" ya da "),
-                Expanded(child: Divider()),
-              ],
-            ),
-            SizedBox(height: 16),
-            ElevatedButton.icon(
-              onPressed: () {
-                AuthService().signInWithGoogle(context);
-              },
-              icon: SvgPicture.asset('lib/assets/icons/google_icon.svg', height: 18),
-              label: Text(''),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.grey[200],
-                side: BorderSide(color: Colors.white),
-                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+              SizedBox(height: 16),
+               TextField(
+                controller: _tPassword,
+                decoration: InputDecoration(
+                  labelText: 'Şifre',
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true,
               ),
-            ),
-            SizedBox(height: 16),
-            Text(
-              'Giriş yap’a tıklayarak enYakın Hizmet Şartlarımızı ve Gizlilik Politikamızı kabul etmiş olursunuz.',
-              style: TextStyle(fontSize: 12 , color: Colors.blueGrey),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 16),
-            TextButton(
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => SignUpPage()));
-              },
-              child: Text(
-                'Hesabın yok mu? Kaydolmak için tıkla!',
-                style: TextStyle(color: mainColor),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                    AuthService().signIn(context, email: _tEmail.text, password: _tPassword.text);
+                },
+                child: Text('Giriş Yap' , style: TextStyle(color: Colors.white),),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: mainColor,
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                ),
               ),
-            ),
-          ],
+              SizedBox(height: 16),
+              Row(
+                children: [
+                  Expanded(child: Divider()),
+                  Text(" ya da "),
+                  Expanded(child: Divider()),
+                ],
+              ),
+              SizedBox(height: 16),
+              ElevatedButton.icon(
+                onPressed: () {
+                  AuthService().signInWithGoogle(context);
+                },
+                icon: SvgPicture.asset('lib/assets/icons/google_icon.svg', height: 18),
+                label: Text(''),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.grey[200],
+                  side: BorderSide(color: Colors.white),
+                  padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                ),
+              ),
+              SizedBox(height: 16),
+              Text(
+                'Giriş yap’a tıklayarak enYakın Hizmet Şartlarımızı ve Gizlilik Politikamızı kabul etmiş olursunuz.',
+                style: TextStyle(fontSize: 12 , color: Colors.blueGrey),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 16),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => SignUpPage()));
+                },
+                child: Text(
+                  'Hesabın yok mu? Kaydolmak için tıkla!',
+                  style: TextStyle(color: mainColor),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
