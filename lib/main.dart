09b70b74fb5a5111f2cd58b7d1bf41391fba5ba6
İsmail Babacan <114854598+ismailbabacann@users.noreply.github.com>
2024-08-13 00:20:16 +0300

    splash_page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:eventfindapp/screens/login_page.dart';
+import 'package:eventfindapp/screens/splash_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:eventfindapp/screens/mainpage.dart';
@@ -21,7 +22,10 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: LoginPage() ,
+      home: SplashPage(),
+      routes: {
+        '/login': (context) => LoginPage(), // LoginPage rotası
+      },
     );
   }
 }
---
+++ lib/screens/splash_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/login');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SvgPicture.asset('lib/assets/icons/splash_blue.svg',
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
